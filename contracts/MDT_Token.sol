054ee9f038df41af749472e23546991bb2326c57
Martin D. <dimitrov.martin.v@gmail.com.>
2025-03-16 19:41:08 +0200

    Added utils with onlyOwner

--- contracts/MDT_Token.sol
+++ contracts/MDT_Token.sol
@@ -3,10 +3,11 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "./ERC20.sol";
+import {Ownable} from "../utils/Ownable.sol";
 
-contract MartinDimitrovToken is ERC20("Martin Dimitrov Token", "MDT") {
+contract MartinDimitrovToken is ERC20("Martin Dimitrov Token", "MDT"), Ownable(msg.sender) {
 
-    function mint(uint amount) public {
+    function mint(uint amount) public onlyOwner {
         _mint(msg.sender, amount * 10 ** decimals());
     }
 }
