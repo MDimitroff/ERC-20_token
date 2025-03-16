2d80433c23096e3f69136e935e3f3a59bf205c51
Martin D. <dimitrov.martin.v@gmail.com.>
2025-03-16 19:23:54 +0200

    Added mint function

--- contracts/MDT_Token.sol
+++ contracts/MDT_Token.sol
@@ -5,5 +5,8 @@ pragma solidity ^0.8.20;
 import {ERC20} from "./ERC20.sol";
 
 contract MartinDimitrovToken is ERC20("Martin Dimitrov Token", "MDT") {
-    
+
+    function mint(uint amount) public {
+        _mint(msg.sender, amount * 10 ** decimals());
+    }
 }
